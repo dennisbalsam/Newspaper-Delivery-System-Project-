cc902e631d75e1af72d10a3ec3dd037c2dee1acf
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-04-25 04:00:17 -0400

    added subscription methods to household class

--- household_test/Program.cs
+++ household_test/Program.cs
@@ -7,8 +7,50 @@ namespace household_test
     {
         static void Main(string[] args)
         {
+            // testing default constructor no params
+            Console.WriteLine("Testing default constructor no params:");
             Household h1 = new Household();
-            Console.WriteLine(h1);
+            Console.WriteLine(h1 + "\n");
+
+            // testing default constructor one param
+            Console.WriteLine("Testing default constructor one param:");
+            Household h2 = new Household("Johnson Smith");
+            Console.WriteLine(h2 + "\n");
+
+            // testing default constructor all params
+            Console.WriteLine("Testing default constructor all params:");
+            Household h3 = new Household("George Brown", "34 State St.", "347828345", true);
+            Console.WriteLine(h3 + "\n");
+
+            // test getter
+            Console.WriteLine("Testing getter:");
+            Console.WriteLine("Suspended: " + h3.Suspended + "\n");
+
+            // test setter
+            Console.WriteLine("Testing setter:");
+            h3.Suspended = false;
+            Console.WriteLine("Suspended: " + h3.Suspended + "\n");
+
+            // test subscription methods
+            h3.addSubscription("Gardening Monthly");
+            h3.addSubscription("The Daily News");
+            h3.addSubscription("Cycling International");
+
+            // check for subscription
+            Console.WriteLine("Checking for 'The Daily News': " + h3.hasSubscription("The Daily News"));
+
+            //  display all subscriptions
+            Console.WriteLine("Display all subscriptions:");
+            Console.WriteLine(h3.getAllSubscriptions() + "\n");
+
+            // remove subscription
+            h3.removeSubscription("The Daily News");
+            //  display all subscriptions again
+            Console.WriteLine("Display all subscriptions after removal:");
+            Console.WriteLine(h3.getAllSubscriptions() + "\n");
+
+            // testing toString with subscriptions
+            Console.WriteLine(h3);
         }
     }
 }
--- newspaper-delivery-system/household.cs
+++ newspaper-delivery-system/household.cs
@@ -10,11 +10,15 @@ namespace newspaper_delivery_system
     // Households subscribe to publications and are delivered to by carriers
     public class Household : Carrier
     {
-        private List<string> subscriptions; // Publications household is subscribed to
+        private List<string> subscriptions = new List<string>(); // Publications household is subscribed to
         private bool suspended; // if household is suspended from receiving delivery
 
         // getter and setter
-        public bool Suspended { get; set; }
+        public bool Suspended
+        {
+            get { return suspended; }
+            set { suspended = value; }
+        }
 
         // default constructor with constructor delegation
         public Household(string _name = "NO_NAME", string _address = "NO_ADDRESS", string _phone = "NO_PHONE", bool _suspended = false) : base(_name, _address, _phone)
@@ -22,10 +26,60 @@ namespace newspaper_delivery_system
             suspended = _suspended;
         }
 
+        // subscription methods
+        // ====================
+
+        // check if household has subscription
+        public bool hasSubscription(string sub)
+        {
+            // subscription to look for
+            string found = subscriptions.Find(item => item == sub);
+
+            if (found == null)
+                return false;
+            return true;
+        }
+
+        // add subscription to list
+        public void addSubscription(string sub)
+        {
+            subscriptions.Add(sub);
+        }
+
+        // remove subscription from list
+        public bool removeSubscription(string sub)
+        {
+            int index = -1;
+            // check if sub exists
+            if (hasSubscription(sub))
+            {
+                index = subscriptions.FindIndex(item => item == sub);
+                subscriptions.RemoveAt(index);
+                return true;
+            }
+            else
+                return false;
+        }
+
+        // display all subscriptions
+        public string getAllSubscriptions()
+        {
+            string allSubs = ""; // hold all subscriptions
+
+            if (subscriptions.Count == 0)
+                return "No subscriptions";
+
+            foreach(string sub in subscriptions)
+            {
+                allSubs += "\t" + sub + "\n";
+            }
+            return allSubs;
+        }
+
         // toString
         public override string ToString()
         {
-            return base.ToString() + "\nSuspended: " + suspended;
+            return base.ToString() + "\nSuspended: " + suspended + "\nSubscriptions: \n" + getAllSubscriptions();
         }
     }
 }
