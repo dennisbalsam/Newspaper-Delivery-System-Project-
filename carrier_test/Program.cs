7aab7b82d37e38703d9f3aa5d9fc050631191476
Matthew Connelly <matthew.b.connelly@gmail.com>
2019-04-25 01:51:44 -0400

    added carrier class and carrier test

---
+++ carrier_test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using newspaper_delivery_system;
+
+namespace carrier_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // testing default constructor with default params
+            Console.WriteLine("Testing default constructor with default params:");
+            Carrier c1 = new Carrier();
+            Console.WriteLine(c1 + "\n");
+
+            // testing default constructor with one param
+            Console.WriteLine("Testing default constructor with one param:");
+            Carrier c2 = new Carrier("United Delivery");
+            Console.WriteLine(c2 + "\n");
+
+            // testing default constructor with all params
+            Console.WriteLine("Testing default constructor with all params:");
+            Carrier c3 = new Carrier("United Delivery", "101 Mulberry Lane", "8008784848");
+            Console.WriteLine(c3 + "\n");
+
+        }
+    }
+}
---
+++ newspaper-delivery-system/carrier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newspaper_delivery_system
+{
+    // Carrier makes deliveries to households
+    public class Carrier
+    {
+        // name, address and phone number
+        private string name, address, phone;
+
+        // default constructor
+        public Carrier(string _name = "NO_NAME", string _address = "NO_ADDRESS", string _phone = "NO_PHONE")
+        {
+            name = _name;
+            address = _address;
+            phone = _phone;
+        }
+
+        // getters and setters
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+        // toString
+        public override string ToString()
+        {
+            return "Name: " + name + "\nAddress: " + address + "\nPhone: " + phone;
+        }
+    }
+}
