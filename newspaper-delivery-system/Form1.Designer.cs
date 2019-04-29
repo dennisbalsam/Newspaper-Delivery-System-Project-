7e781cff728ff58f97e7f1886f09d566e79bf652
Dennis Krupitsky <d.krupitsky@gmail.com>
2019-04-28 20:56:34 -0400

    Updated code

--- newspaper-delivery-system/Form1.Designer.cs
+++ newspaper-delivery-system/Form1.Designer.cs
@@ -29,8 +29,10 @@
         private void InitializeComponent()
         {
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.Welcomelabel = new System.Windows.Forms.Label();
+            this.welcomelabel2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataList = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -41,42 +43,69 @@
             "Household Info",
             "Carriers",
             "Publications"});
-            this.comboBox1.Location = new System.Drawing.Point(336, 96);
+            this.comboBox1.Location = new System.Drawing.Point(344, 107);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(762, 21);
             this.comboBox1.TabIndex = 0;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
-            // label1
+            // Welcomelabel
             // 
-            this.label1.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(476, 24);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(483, 23);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Welcome to the Newspaper Delivery System";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Welcomelabel.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Welcomelabel.Location = new System.Drawing.Point(476, 24);
+            this.Welcomelabel.Name = "Welcomelabel";
+            this.Welcomelabel.Size = new System.Drawing.Size(483, 23);
+            this.Welcomelabel.TabIndex = 1;
+            this.Welcomelabel.Text = "Welcome to the Newspaper Delivery System";
+            this.Welcomelabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Welcomelabel.Click += new System.EventHandler(this.Welcomelabel_Click);
             // 
-            // label2
+            // welcomelabel2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(619, 57);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(196, 20);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Please select an option";
+            this.welcomelabel2.AutoSize = true;
+            this.welcomelabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomelabel2.Location = new System.Drawing.Point(619, 57);
+            this.welcomelabel2.Name = "welcomelabel2";
+            this.welcomelabel2.Size = new System.Drawing.Size(196, 20);
+            this.welcomelabel2.TabIndex = 2;
+            this.welcomelabel2.Text = "Please select an option";
+            this.welcomelabel2.Click += new System.EventHandler(this.welcomelabel2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(623, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataList
+            // 
+            this.dataList.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.dataList.FormattingEnabled = true;
+            this.dataList.Location = new System.Drawing.Point(402, 265);
+            this.dataList.Name = "dataList";
+            this.dataList.Size = new System.Drawing.Size(671, 95);
+            this.dataList.TabIndex = 4;
+            this.dataList.Visible = false;
+            this.dataList.SelectedIndexChanged += new System.EventHandler(this.dataList_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1434, 550);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataList);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.welcomelabel2);
+            this.Controls.Add(this.Welcomelabel);
             this.Controls.Add(this.comboBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,8 +114,10 @@
         #endregion
 
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Welcomelabel;
+        private System.Windows.Forms.Label welcomelabel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox dataList;
     }
 }
 
--- newspaper-delivery-system/Form1.cs
+++ newspaper-delivery-system/Form1.cs
@@ -19,10 +19,101 @@ namespace newspaper_delivery_system
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (comboBox1.SelectedIndex.ToString() == "Household Info")
+            if (comboBox1.SelectedItem.ToString() == "Household Info")
             {
-
+                HouseholdInfo();
             }
+            else if (comboBox1.SelectedItem.ToString() == "Carriers")
+            {
+                CarrierInfo();
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Publications")
+            {
+                PublicationInfo();
+            }
+        }
+
+        public void HouseholdInfo()
+        {
+            dataList.Visible = false;
+            welcomelabel2.Visible = false;
+            Welcomelabel.Visible = false;
+            button1.Visible = true;
+            button1.Text = "Display all Households";
+            button1.Click += new EventHandler(DisplayallHouseinfo);
+        }
+
+        public void CarrierInfo()
+        {
+            dataList.Visible = false;
+            welcomelabel2.Visible = false;
+            Welcomelabel.Visible = false;
+            button1.Visible = true;
+            button1.Text = "Display all Carriers";
+            button1.Click += new EventHandler(DisplayallCarrierinfo);
+        }
+
+        public void PublicationInfo()
+        {
+            dataList.Visible = false;
+            welcomelabel2.Visible = false;
+            Welcomelabel.Visible = false;
+            button1.Visible = true;
+            button1.Text = "Display all Publications";
+            button1.Click += new EventHandler(DisplayallPublicationinfo);
+        }
+
+        public void DisplayallHouseinfo(object sender, EventArgs e)
+        {
+
+            string path = @"..\..\..\";
+            List<Household> households = DataHandler.getHouseholdData(path + "households.csv");
+            dataList.DataSource = households;
+            dataList.Visible = true;
+
+        }
+
+        public void DisplayallCarrierinfo(object sender, EventArgs e)
+        {
+            string path = @"..\..\..\";
+            List<Carrier> carriers = DataHandler.getCarrierData(path + "carriers.csv");
+            dataList.DataSource = carriers;
+            dataList.Visible = true;
+
+        }
+
+
+        public void DisplayallPublicationinfo(object sender, EventArgs e)
+        {
+            string path = @"..\..\..\";
+            List<Publication> publications = DataHandler.getPublicationData(path + "publications.csv");
+            dataList.DataSource = publications;
+            dataList.Visible = true;
+
+        }
+        private void welcomelabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Welcomelabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
