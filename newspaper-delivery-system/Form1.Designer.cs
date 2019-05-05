faae0111546a683ac7ef506dd68175a74781fc42
Dennis Krupitsky <d.krupitsky@gmail.com>
2019-05-04 21:44:19 -0400

    Update

--- newspaper-delivery-system/Form1.Designer.cs
+++ newspaper-delivery-system/Form1.Designer.cs
@@ -182,7 +182,7 @@
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.messageBox);
-            this.panel1.Location = new System.Drawing.Point(252, 772);
+            this.panel1.Location = new System.Drawing.Point(261, 772);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(780, 55);
             this.panel1.TabIndex = 11;
